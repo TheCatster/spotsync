9e0741d289c7adcd1bad5f248f7ce46693bccb34
Daniel Rose <danielrose@member.fsf.org>
2021-08-20 21:54:10 -0400

    Remove tests file

--- src/tests.rs
+++
@@ -1,9 +0,0 @@
-#[test]
-fn read_refresh_token() {
-    let _file = std::fs::read_to_string(".refresh_token");
-}
-
-#[test]
-fn get_authenticated_user() {
-    let _client = super::authenticate_spotify();
-}
